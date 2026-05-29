748370cb88c50694d7d5719697f6660117403730
m25359558-commits <m25359558@gmail.com>
2026-05-29 16:01:23 +0500

    Update main.py

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -242,7 +242,8 @@ STUDENT_DATA = {
 
 # 2. Данные ПРЕПОДАВАТЕЛЯ
 TEACHER_DATA = {
-
+"m25359558@gmail.com
+" :{
     "role": "teacher",
     # личная информация 
     "personal_info": {
@@ -388,7 +389,7 @@ TEACHER_DATA = {
   "disc_lang_3": "Русский, Казахский",
   "disc_period_3": "1 семестр"
 }
-
+},
 }
 
 # Основная функция обработки
